fd1953a31ab1aea066d1700c47e00d960ceed176
lucassmann <lucas.assmann@edu.pucrs.br>
2022-08-16 20:20:50 -0300

    added try, catch so sort and get methods return empty Arraylist<Person> when PersonStorage is empty.

--- src/main/java/com/example/devscool/PeopleService.java
+++ src/main/java/com/example/devscool/PeopleService.java
@@ -14,38 +14,70 @@ public class PeopleService {
     }
 
     public List<Person> sortByName(){
-        return personStorage.getAll().stream()
-                .sorted(Comparator.comparing(Person::getName))
-                .collect(Collectors.toList());
+        try{
+            return personStorage.getAll().stream()
+                    .sorted(Comparator.comparing(Person::getName))
+                    .collect(Collectors.toList());
+        }
+        catch (NullPointerException e){
+            return this.personStorage.getAll();
+        }
     }
 
     public List<Person> sortByAge(){
-        return personStorage.getAll().stream()
-                .sorted(Comparator.comparing(Person::getAge))
-                .collect(Collectors.toList());
+        try {
+            return personStorage.getAll()
+                    .stream()
+                    .sorted(Comparator.comparing(Person::getAge))
+                    .collect(Collectors.toList());
+        }
+        catch (NullPointerException e){
+            return this.personStorage.getAll();
+        }
     }
 
     public List<Person> getChildren(){
-        return personStorage.getAll().stream()
-                .filter(person -> (person.getAge() < 12))
-                .collect(Collectors.toList());
+        try {
+            return personStorage.getAll().stream()
+                    .filter(person -> (person.getAge() < 12))
+                    .collect(Collectors.toList());
+        }
+        catch (NullPointerException e){
+            return this.personStorage.getAll();
+        }
+
     }
 
     public List<Person> getTeenagers(){
-        return personStorage.getAll().stream()
-                .filter(person -> (person.getAge() >= 12 && person.getAge() < 20))
-                .collect(Collectors.toList());
+        try {
+            return personStorage.getAll().stream()
+                    .filter(person -> (person.getAge() >= 12 && person.getAge() < 20))
+                    .collect(Collectors.toList());
+        }
+        catch (NullPointerException e){
+            return this.personStorage.getAll();
+        }
     }
 
     public List<Person> getAdults(){
-        return personStorage.getAll().stream()
-                .filter(person -> (person.getAge() >= 20 && person.getAge() < 65))
-                .collect(Collectors.toList());
+        try {
+            return personStorage.getAll().stream()
+                    .filter(person -> (person.getAge() >= 20 && person.getAge() < 65))
+                    .collect(Collectors.toList());
+        }
+        catch (NullPointerException e){
+            return this.personStorage.getAll();
+        }
     }
 
     public List<Person> getElderly(){
-        return personStorage.getAll().stream()
-                .filter(person -> (person.getAge() > 65))
-                .collect(Collectors.toList());
+        try {
+            return personStorage.getAll().stream()
+                    .filter(person -> (person.getAge() > 65))
+                    .collect(Collectors.toList());
+        }
+        catch (NullPointerException e){
+            return this.personStorage.getAll();
+        }
     }
 }
